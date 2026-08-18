699722f7fa54e48b3c27a93c57f30f3a5764b9bd
abdeljalilelmisbahi-crypto <abdeljalilelmisbahi@gmail.com>
2026-08-18 13:11:29 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Ana mart mbi a w9", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
         type: 1,               // Custom
       },
